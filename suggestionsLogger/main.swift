68e541a7fe1b017e3e3b554dca0f42f2cbedee6c
Quentin Roy <quentin@quentinroy.fr>
2019-11-06 15:39:43 -0500

    Initial Commit

---
+++ suggestionsLogger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  suggestionsLogger
+//
+//  Created by Quentin Roy on 2019-11-06.
+//  Copyright © 2019 Quentin Roy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
